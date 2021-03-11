d8c42cb2575365afdc0fd5df55a964dd798036b3
Anth Richardson <anthrblahblah@gmail.com>
2021-03-11 23:28:41 +0000

    Added memcache test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cache from 'memory-cache';
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  const value = cache.get('count');
+  let count;
+
+  if(!value) {
+    count = 1;
+  } else {
+    count = parseInt(value)
+    count += 1;
+  }
+  
+  cache.put('count', count);
+  res.status(200).json({count})
 }
