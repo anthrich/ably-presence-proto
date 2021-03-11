f40f7b7ba31174cb1ebab64c81cc4b82028ebb8e
Anth Richardson <anthrblahblah@gmail.com>
2021-03-11 17:39:51 +0000

    Switched auth to Ably.Rest

--- pages/api/createTokenRequest.js
+++ pages/api/createTokenRequest.js
@@ -2,7 +2,7 @@ import Ably from "ably/promises";
 import randomColor from 'randomcolor';
 
 export default async function handler(req, res) {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+    const client = new Ably.Rest({ key: process.env.ABLY_API_KEY });
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: randomColor() });
     res.status(200).json(tokenRequestData);
 };
